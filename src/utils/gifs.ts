dbb4322f3fd6a813813bb2036a143c275002a173
sirbdhy46-alt <sirbdhy46@gmail.com>
2026-04-10 03:48:11 +0530

    Update gifs.ts

--- src/utils/gifs.ts
+++ src/utils/gifs.ts
@@ -4,6 +4,20 @@ export const GIFS: Record<string, string[]> = {
     "https://media.tenor.com/4Ic42JYUZQIAAAAC/banned-ban.gif",
     "https://media.tenor.com/deWpKiY6MVMAAAAC/ban-hammer-banned.gif",
   ],
+  hardban: [
+    "https://media.tenor.com/KvHiT-bHMOgAAAAC/explosion-nuke.gif",
+    "https://media.tenor.com/2hL6d2MrdWwAAAAC/boom-explosion.gif",
+    "https://media.tenor.com/1-VN5FdFjPkAAAAC/nuke-nuclear.gif",
+    "https://media.tenor.com/deWpKiY6MVMAAAAC/ban-hammer-banned.gif",
+    "https://media.tenor.com/GnCa5YGsOjMAAAAC/ban-hammer.gif",
+  ],
+  hardfuck: [
+    "https://media.tenor.com/KvHiT-bHMOgAAAAC/explosion-nuke.gif",
+    "https://media.tenor.com/2hL6d2MrdWwAAAAC/boom-explosion.gif",
+    "https://media.tenor.com/xHjzSRGqDtkAAAAC/anime-fight.gif",
+    "https://media.tenor.com/Zg4ORaJesPkAAAAC/anime-fight.gif",
+    "https://media.tenor.com/vvGiU7-Jh1cAAAAC/fight-anime.gif",
+  ],
   kick: [
     "https://media.tenor.com/aSe5ZbQRNJQAAAAC/kick-epic.gif",
     "https://media.tenor.com/a3WD_dVYBbYAAAAC/fry-kick.gif",
@@ -38,6 +52,8 @@ export const GIFS: Record<string, string[]> = {
     "https://media.tenor.com/NN0LQBWzHfgAAAAC/hug-cute.gif",
     "https://media.tenor.com/C_SiEVB5XrIAAAAC/hug-love.gif",
     "https://media.tenor.com/A7YhsO5HJMoAAAAC/hug-hugs.gif",
+    "https://media.tenor.com/KdKI9tpNrI0AAAAC/anime-hug.gif",
+    "https://media.tenor.com/0HxMnq_HxqMAAAAC/hug-anime.gif",
   ],
   slap: [
     "https://media.tenor.com/pQ76hMkHF_kAAAAC/slap-anime.gif",
@@ -48,6 +64,8 @@ export const GIFS: Record<string, string[]> = {
     "https://media.tenor.com/VRl1SuGvwroAAAAC/kiss-anime.gif",
     "https://media.tenor.com/5dLOD0bBZGUAAAAC/anime-kiss.gif",
     "https://media.tenor.com/eEp7klRHDC8AAAAC/kiss-anime.gif",
+    "https://media.tenor.com/3QGwcJeJifsAAAAC/anime-kiss.gif",
+    "https://media.tenor.com/YBMCwMdx4xMAAAAC/kiss-anime-kiss.gif",
   ],
   dance: [
     "https://media.tenor.com/UxpPPi5HFPIAAAAC/dance-moves.gif",
@@ -64,56 +82,70 @@ export const GIFS: Record<string, string[]> = {
     "https://media.tenor.com/pItLfmIm3IMAAAAC/robbing.gif",
     "https://media.tenor.com/AZO3LfX0f04AAAAC/robbery-robbing.gif",
   ],
+  ship: [
+    "https://media.tenor.com/B6XrKKaT2IoAAAAC/hearts-love.gif",
+    "https://media.tenor.com/eRPmfXVVSW0AAAAC/pink-hearts.gif",
+    "https://media.tenor.com/HEARTS-love-anime.gif",
+    "https://media.tenor.com/VRl1SuGvwroAAAAC/kiss-anime.gif",
+    "https://media.tenor.com/5dLOD0bBZGUAAAAC/anime-kiss.gif",
+    "https://media.tenor.com/nG2MLb-GqgMAAAAC/butterflies-butterfly.gif",
+    "https://media.tenor.com/F7avU-7PQHQAAAAC/moon-stars.gif",
+  ],
+  pat: [
+    "https://media.tenor.com/LAmHNbFHOgAAAAC/pat-anime.gif",
+    "https://media.tenor.com/GlqgcJOyW1IAAAAC/pat-head.gif",
+    "https://media.tenor.com/B1l-dXkHnH8AAAAC/anime-pat.gif",
+  ],
+  poke: [
+    "https://media.tenor.com/aBNnkQfHJioAAAAC/poke-anime.gif",
+    "https://media.tenor.com/0IiCHkWFT0cAAAAC/poke-annoying.gif",
+    "https://media.tenor.com/JCWJ3NiHu3MAAAAC/poke-discord.gif",
+  ],
+  cry: [
+    "https://media.tenor.com/8kSMpZ8yDW0AAAAC/lana-del-rey-summertime-sadness.gif",
+    "https://media.tenor.com/wVikCn4TT4IAAAAC/anime-crying.gif",
+    "https://media.tenor.com/nN5kxCcFCVcAAAAC/cry-anime.gif",
+  ],
 };
 
 export const WELCOME_GIFS = [
-  // Sparkle & glitter
   "https://media.tenor.com/nEpCECMUv_UAAAAC/glitter-sparkle.gif",
   "https://media.tenor.com/H_BqKYuHe4MAAAAC/sparkle-stars.gif",
   "https://media.tenor.com/uiRFScbmpUEAAAAC/sparkles-glitter.gif",
   "https://media.tenor.com/8mFyTbKe3DAAAAAC/sparkle-rainbow.gif",
-  // Party & celebration
   "https://media.tenor.com/wj3G0HnMgesAAAAC/party-celebrate.gif",
   "https://media.tenor.com/VgcH3GNVEusAAAAC/confetti-celebrate.gif",
   "https://media.tenor.com/q1ZBxCz4i8MAAAAC/party-confetti.gif",
   "https://media.tenor.com/Yp_7vFxRQiIAAAAC/celebration-party.gif",
   "https://media.tenor.com/4GFfHRMRrvcAAAAC/celebrate-confetti.gif",
-  // Aesthetic & dreamy
   "https://media.tenor.com/R4JVWZ9aVhYAAAAC/aesthetic-dreamy.gif",
   "https://media.tenor.com/b5fWCvF1K9AAAAAC/aesthetic-pink.gif",
   "https://media.tenor.com/LvXiIgLm0UYAAAAC/aesthetic-flowers.gif",
   "https://media.tenor.com/9vZSo2F3dUoAAAAC/pink-aesthetic.gif",
   "https://media.tenor.com/IfMSQI5YXTIAAAAC/aesthetic-sky.gif",
-  // Cute & kawaii
   "https://media.tenor.com/EopJSepHCCsAAAAC/cute-kawaii.gif",
   "https://media.tenor.com/oSiZfX7OFoMAAAAC/cute-bunny.gif",
   "https://media.tenor.com/vBrNT7Jqhd4AAAAC/anime-cute.gif",
   "https://media.tenor.com/oBP-g0HJGXIAAAAC/kawaii-cute.gif",
   "https://media.tenor.com/6t3JhKJ3GNYAAAAC/cute-cat.gif",
-  // Flowers & nature
   "https://media.tenor.com/OlNsHJfNg4YAAAAC/flowers-blooming.gif",
   "https://media.tenor.com/ZzEq5jV2gBYAAAAC/cherry-blossom.gif",
   "https://media.tenor.com/Zv2ZVrX0N8QAAAAC/flower-bloom.gif",
   "https://media.tenor.com/wFfbFSHNj8IAAAAC/roses-flowers.gif",
-  // Stars & moon
   "https://media.tenor.com/JMpzSH1k38MAAAAC/stars-night.gif",
   "https://media.tenor.com/F7avU-7PQHQAAAAC/moon-stars.gif",
   "https://media.tenor.com/kMXIyKyULLIAAAAC/shooting-star.gif",
   "https://media.tenor.com/VPGS6JdF1q0AAAAC/stars-galaxy.gif",
-  // Lana vibes
   "https://media.tenor.com/MiMJrP-V5l0AAAAC/lana-del-rey-blue.gif",
   "https://media.tenor.com/vx1WnpWq8ZUAAAAC/lana-del-rey.gif",
   "https://media.tenor.com/WkJkwz0ADPQAAAAC/lana-del-rey-sad.gif",
   "https://media.tenor.com/WXVqDYNjN18AAAAC/lana-del-rey-ride.gif",
   "https://media.tenor.com/8kSMpZ8yDW0AAAAC/lana-del-rey-summertime-sadness.gif",
-  // Rainbow & colorful
   "https://media.tenor.com/WJGR0kRkJpgAAAAC/rainbow-colorful.gif",
   "https://media.tenor.com/VEb2T5v-1GMAAAAC/colorful-rainbow.gif",
   "https://media.tenor.com/VJVFz5JVARYAAAAC/neon-lights.gif",
-  // Butterflies
   "https://media.tenor.com/bGG0iGzLWkMAAAAC/butterfly-butterflies.gif",
   "https://media.tenor.com/nG2MLb-GqgMAAAAC/butterflies-butterfly.gif",
-  // Hearts & love
   "https://media.tenor.com/B6XrKKaT2IoAAAAC/hearts-love.gif",
   "https://media.tenor.com/eRPmfXVVSW0AAAAC/pink-hearts.gif",
   "https://media.tenor.com/Xf_nnfgCwuIAAAAC/hearts-heart.gif",
@@ -147,18 +179,18 @@ export const LANA_GIFS = [
 
 export function getGif(type: string): string {
   const list = GIFS[type];
-  if (!list) return "";
-  return list[Math.floor(Math.random() * list.length)];
+  if (!list || list.length === 0) return "";
+  return list[Math.floor(Math.random() * list.length)]!;
 }
 
 export function getLanaGif(): string {
-  return LANA_GIFS[Math.floor(Math.random() * LANA_GIFS.length)];
+  return LANA_GIFS[Math.floor(Math.random() * LANA_GIFS.length)]!;
 }
 
 export function getWelcomeGif(): string {
-  return WELCOME_GIFS[Math.floor(Math.random() * WELCOME_GIFS.length)];
+  return WELCOME_GIFS[Math.floor(Math.random() * WELCOME_GIFS.length)]!;
 }
 
 export function getWelcomeMessage(): string {
-  return WELCOME_MESSAGES[Math.floor(Math.random() * WELCOME_MESSAGES.length)];
+  return WELCOME_MESSAGES[Math.floor(Math.random() * WELCOME_MESSAGES.length)]!;
 }
